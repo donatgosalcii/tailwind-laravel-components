6e3af487adbc10a4936d21e08317b4e9ad483fca
dgwie2k <gosalci38@gmail.com>
2025-07-10 14:48:33 +0200

    Fixed a visual issue on the FAQ page

--- resources/views/pages/faq.blade.php
+++ resources/views/pages/faq.blade.php
@@ -3,31 +3,39 @@
 @section('content')
     <div class="bg-white dark:bg-slate-900">
         <div class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
-            <div class="mx-auto max-w-4xl divide-y divide-gray-900/10 dark:divide-white/10">
-                <h2 class="text-4xl font-bold leading-10 tracking-tight text-gray-900 dark:text-white">Frequently asked questions</h2>
+            <div class="mx-auto max-w-4xl">
+                <h2 class="text-4xl font-bold leading-10 tracking-tight text-gray-900 dark:text-white">
+                    Frequently asked questions
+                </h2>
 
                 <dl class="mt-10 space-y-6 divide-y divide-gray-900/10 dark:divide-white/10">
-
                     <div x-data="{ open: false }" class="pt-6">
                         <dt>
-                            <button @click="open = !open" type="button" class="flex w-full items-start justify-between text-left text-gray-900 dark:text-white">
+                            <button @click="open = !open" type="button"
+                                    class="flex w-full items-start justify-between text-left text-gray-900 dark:text-white">
                                 <span class="text-base font-semibold leading-7">What's the best thing about Switzerland?</span>
                                 <span class="ml-6 flex h-7 items-center">
-                                    <svg x-show="open" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                    <svg x-show="open" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M18 12H6" />
                                     </svg>
-                                    <svg x-show="!open" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m6-6H6" />
+                                    <svg x-show="!open" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                              d="M12 6v12m6-6H6" />
                                     </svg>
                                 </span>
                             </button>
                         </dt>
                         <dd x-show="open" x-collapse class="mt-2 pr-12">
-                            <p class="text-base leading-7 text-gray-600 dark:text-gray-300">I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</p>
+                            <p class="text-base leading-7 text-gray-600 dark:text-gray-300">
+                                I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Quas cupiditate laboriosam fugiat.
+                            </p>
                         </dd>
                     </div>
 
-                    <div x-data="{ open: false }" class="pt-6">
+                    <div x-data="{ open: false }" class="pt-6 border-t-0">
                         <dt>
                             <button @click="open = !open" type="button" class="flex w-full items-start justify-between text-left text-gray-900 dark:text-white">
                                 <span class="text-base font-semibold leading-7">How do you make holy water?</span>
